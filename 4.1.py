bfd863e61a5de5073175425e025d13b2dd10930b
Krysti4n <68149494+xKrysti4n@users.noreply.github.com>
2024-11-27 21:58:35 +0000

    commit

--- 4.1.py
+++ 4.1.py
@@ -9,3 +9,7 @@ for pojedyncze_slowo in slowa:
     ile_k = re.findall(r'k', pojedyncze_slowo)
     if len(ile_w) == len(ile_k):
         print(pojedyncze_slowo)
+
+
+# https://www.korepetycjezinformatyki.pl/wp-content/uploads/nowa-rozszerzona/informatyka-2023-maj-matura-rozszerzona-2.pdf
+# https://www.korepetycjezinformatyki.pl/wp-content/uploads/nowa-rozszerzona/informatyka-2023-maj-matura-rozszerzona-odpowiedzi.pdf
